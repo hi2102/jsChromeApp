5b42ba8bf91a3f4c05c92c90405ab3baa7938df8
hi2102 <hi2102@naver.com>
2022-07-05 23:35:11 +0900

    css추가설정커밋!

--- nomad/js/backGroud.js
+++ nomad/js/backGroud.js
@@ -4,7 +4,8 @@ const images = [
   'img02.jpg',
   'img03.jpg',
   'img04.jpg',
-  'img05.jpg'
+  'img05.jpg',
+  'img06.jpg',
 ];
 
 const chosenImage = images[Math.floor(Math.random() * images.length)];
